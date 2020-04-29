3a53a9de8bacfce88ac4e8d94a524aa7ff930bee
MeliiZa <melissazandman@icloud.com>
2020-04-29 15:12:13 -0700

    Add animation when hover and modal when click

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(function () {
-  $('[data-toggle="modal"]').hover(function () {
+  $('[data-toggle="modal"]').onClick(function () {
     var modalId = $(this).data("target");
     $(modalId).modal("show");
   });
